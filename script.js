bc6c574e8f3b1feacb03124a19a6760a9ec27cd0
Sheikh Aamir <113338901+aamirk0008@users.noreply.github.com>
2025-07-12 22:53:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,61 +1,43 @@
 //complete this code
-class Person {
-    private _name: string;
-    private _age: number;
+ class Person {
+            constructor(name, age) {
+                this._name = name;
+                this._age = age;
+            }
 
-    constructor(name: string, age: number) {
-        this._name = name;
-        this._age = age;
-    }
+            get name() {
+                return this._name;
+            }
 
-    // Name getter
-    get name(): string {
-        return this._name;
-    }
+            get age() {
+                return this._age;
+            }
 
-    // Age getter
-    get age(): number {
-        return this._age;
-    }
+            set age(age) {
+                this._age = age;
+            }
+        }
 
-    // Age setter
-    set age(age: number) {
-        this._age = age;
-    }
-}
+        class Student extends Person {
+            constructor(name, age) {
+                super(name, age);
+            }
 
-class Student extends Person {
-    constructor(name: string, age: number) {
-        super(name, age);
-    }
+            study() {
+                console.log(`${this.name} is studying`);
+            }
+        }
 
-    study(): void {
-        console.log(`${this.name} is studying`);
-    }
-}
+        class Teacher extends Person {
+            constructor(name, age) {
+                super(name, age);
+            }
 
-class Teacher extends Person {
-    constructor(name: string, age: number) {
-        super(name, age);
-    }
+            teach() {
+                console.log(`${this.name} is teaching`);
+            }
+        }
 
-    teach(): void {
-        console.log(`${this.name} is teaching`);
-    }
-}
-
-// Example usage:
-const person = new Person("John", 25);
-console.log(person.name);  // Output: John
-
-person.age = 30;  // Using the setter to change the age
-console.log(person.age);  // Output: 30
-
-const student = new Student("Alice", 22);
-student.study();  // Output: Alice is studying
-
-const teacher = new Teacher("Bob", 40);
-teacher.teach();  // Output: Bob is teaching
 
 // Do not change the code below this line
 window.Person = Person;
